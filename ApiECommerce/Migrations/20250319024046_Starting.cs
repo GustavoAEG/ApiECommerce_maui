96d11da6a93ee045ceb5bae19c7b5ff187cb23de
Gustavo <Gustavo@WIN-216HVE2K6U1>
2025-03-18 23:52:38 -0300

    FEAT: Configurações Iniciais

---
+++ ApiECommerce/Migrations/20250319024046_Starting.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiECommerce.Migrations
+{
+    /// <inheritdoc />
+    public partial class Starting : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
